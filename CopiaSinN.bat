b7d98645b19acaef8a17a36a2a0acf55167a6217
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-02-02 17:24:11 -0600

    Update CopiaSinN.bat
    Copia entre dos direcciones, excluyendo los archivos que desee

--- CopiaSinN.bat
+++ CopiaSinN.bat
@@ -1,18 +1,18 @@
 @echo off
 
-:Configuración
 set "origen=C:\programacionbatch\Origen"
 set "destino=C:\programacionbatch\Destino"
 set "archivos_a_borrar=nocopiar.txt nocopiar1.txt"
 
-:Copia de archivos y subdirectorios
+::Copia de archivos y subdirectorios /e = copia subdirectorios vacíos 
+::/i = si no existe el directorio lo crea /y = sobreescribe sin preguntar
 xcopy %origen% %destino% /e /i /y
 
-:Borrado de archivos y carpetas
+::Borrado de archivos y carpetas /f = forzar /q = silencioso 
+::/s = subdirectorios
+:: %%a iterador for usa %%
 for %%a in (%archivos_a_borrar%) do (
   echo Borrando "%%a"
   del /f /q /s "%destino%\%%a" 2>nul
 )
-
-echo Proceso completado.
 pause
