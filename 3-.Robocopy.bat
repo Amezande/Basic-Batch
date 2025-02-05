e73d4aae96e9e41e4bb5e0fc3ad57d71ba2ee864
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-02-04 19:27:15 -0600

    Copia Selectiva

--- 3-.Robocopy.bat
+++ 3-.Robocopy.bat
@@ -1,10 +1,12 @@
 @echo off
-Set Ruta=
-set Fuente=
+Set Ruta="C:ruta Fuente"
+set Fuente="C:ruta Destino"
 
+Rem Copia excluyendo todas las carpetas contenidas aqui
 robocopy %fuente %ruta /XD  "C:\ruta\Carpeta1" ^
                             "C:\ruta\Carpeta2"
 
+Rem Copia excluyendo todos los archivos contenidos aquí 
 robocopy %fuente %ruta /XF  "C:\ruta\File1" ^ 
                             "C:\ruta\File2"
 
