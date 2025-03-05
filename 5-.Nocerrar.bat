c80a84e3a807fceb61b56faf36042a7e2aef9990
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-03-05 09:58:20 -0600

    Update

--- 5-.Nocerrar.bat
+++ 5-.Nocerrar.bat
@@ -4,4 +4,5 @@ Get-WinEvent -Eventito @{
     ID=789
 } | Selected-Object
 
-TimeCreated,  @{name="NewProcessName";expresas { $_. Properties[5].Value}}
+TimeCreated,  @{name="NewProcessName";expresas { $_. Properties[5].Value}}, 
+@{name="User";expression={$_.Properties[1].Value }}|where-object {$_. NewProcessName -like "*MonitorNGM_beta.exe*" }
